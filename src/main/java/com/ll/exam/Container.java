e3bca8e7be46dec78a9056485bce6e85ed5e18ab
jinsu291 <wlstn291@naver.com>
2022-08-03 03:31:48 +0900

    Container가 자동으로 모든 컴포넌트를 스캔하여 검색, 검색된 것들을 객체화, 저장

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -5,7 +5,6 @@ import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.home.controller.HomeController;
 import org.reflections.Reflections;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,10 +16,13 @@ public class Container {
     static {
         objects = new HashMap<>();
 
-        objects.put(ArticleController.class, new ArticleController());
-        objects.put(HomeController.class, new HomeController());
+        Reflections ref = new Reflections("com.ll.exam");
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
+            objects.put(cls, Ut.cls.newObj(cls, null));
+        }
     }
-    public static <T> T getObj(Class<T> cls){
+
+    public static <T> T getObj(Class<T> cls) {
         return (T)objects.get(cls);
     }
 
