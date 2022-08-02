6a3763f13ed2567875dd1dbc24451e5f6aae5c3f
jinsu291 <wlstn291@naver.com>
2022-08-03 02:00:11 +0900

    컴포넌트 스캔

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -1,21 +1,42 @@
 package com.ll.exam;
 
+import com.ll.exam.annotation.Controller;
 import com.ll.exam.article.controller.ArticleController;
+import com.ll.exam.home.controller.HomeController;
+import org.reflections.Reflections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Container {
-    private  static final  ArticleController articleController;
+    private static final ArticleController articleController;
+    private static final HomeController homeController;
 
     static {
         articleController = new ArticleController();
+        homeController = new HomeController();
     }
 
     public static ArticleController getArticleController() {
-        return  articleController;
+        return articleController;
+    }
+
+    public static HomeController getHomeController() {
+        return homeController;
     }
 
     public static List<String> getControllerNames() {
-        return List.of("home", "article");
+        List<String> names = new ArrayList<>();
+
+        Reflections ref = new Reflections("com.ll.exam");
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
+            String name = cls.getSimpleName(); // HomeController
+            name = name.replace("Controller", ""); // Home
+            name = Ut.str.decapitalize(name); // home
+
+            names.add(name.replace("Controller", name));
+        }
+
+        return names;
     }
 }
---
+++ src/main/java/com/ll/exam/member/controller/MemberController.java
@@ -0,0 +1,7 @@
+package com.ll.exam.member.controller;
+
+import com.ll.exam.annotation.Controller;
+
+@Controller
+public class MemberController {
+}
