d4d707f28a3d52ed6c51918ab2e5ce86448e0348
jinsu291 <wlstn291@naver.com>
2022-08-03 10:10:32 +0900

    서비스도 컴포넌트로 지정하여 스캔

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -1,6 +1,7 @@
 package com.ll.exam;
 
 import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.Service;
 import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.home.controller.HomeController;
 import org.reflections.Reflections;
@@ -16,11 +17,27 @@ public class Container {
     static {
         objects = new HashMap<>();
 
+        scanComponents();
+        }
+
+    private static void scanComponents() {
+        scanServices();
+        scanControllers();
+    }
+
+    private static void scanServices() {
+        Reflections ref = new Reflections("com.ll.exam");
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Service.class)) {
+            objects.put(cls, Ut.cls.newObj(cls, null));
+        }
+    }
+
+    private static void scanControllers() {
         Reflections ref = new Reflections("com.ll.exam");
         for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
             objects.put(cls, Ut.cls.newObj(cls, null));
         }
-    }
+}
 
     public static <T> T getObj(Class<T> cls) {
         return (T)objects.get(cls);
--- src/main/java/com/ll/exam/service/ArticleService.java
+++ src/main/java/com/ll/exam/service/ArticleService.java
@@ -1,4 +1,7 @@
 package com.ll.exam.service;
 
+import com.ll.exam.annotation.Service;
+
+@Service
 public class ArticleService {
 }
